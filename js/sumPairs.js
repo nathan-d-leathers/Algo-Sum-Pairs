66bb6416c9f19ed986fca7d5c638ec0136aa32c8
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-05-26 12:52:40 -0700

    solved

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,14 @@
-exports.sumPairs = function() {
+exports.sumPairs = function(arr, num) {
+    let result = [];
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === num) {
+                result.push([arr[i], arr[j]]);
+            }
+        }
+    }
+    if (result.length === 0) return 'unable to find pairs';
+    else return result;
+
 
 };
--- js/sumPairsSpec.js
+++ js/sumPairsSpec.js
@@ -1,3 +1,10 @@
 var sp = require("./sumPairs");
 
+console.log(sp.sumPairs([1,2,3,4,5], 9))
+console.log(sp.sumPairs([1,2,3,4,5], 7))
+console.log(sp.sumPairs([3,1,5,8,2], 27))
+
 // Don't forget to add your tests :)
+// sum_pairs([1,2,3,4,5], 9) 
+// sum_pairs([1,2,3,4,5], 7) 
+// sum_pairs([3, 1, 5, 8, 2], 27) 
